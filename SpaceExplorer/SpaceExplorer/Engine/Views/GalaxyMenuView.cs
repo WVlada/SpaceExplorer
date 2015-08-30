48b629a1c5e91e19c7bcb5a4d7f6e6f41e43da30
Vladimir Bukvić <vladimir.bukvic@live.com>
2015-08-30 16:43:18 +0200

    Prvi meni i ogranicenje kretanja u Galaksiji i Sistemu

--- SpaceExplorer/SpaceExplorer/Engine/Views/GalaxyMenuView.cs
+++ SpaceExplorer/SpaceExplorer/Engine/Views/GalaxyMenuView.cs
@@ -11,21 +11,27 @@ namespace SpaceExplorer.Engine
     class GalaxyMenuView : MenuToolbarView
     {
         public static Texture2D teksturaMenija;
+        public static SpriteFont menuItemsFont;
+        public string[] menuItems;
 
         public GalaxyMenuView()
         {
-        
+            this.menuItems = new string[] {"Ships name", "Save game", "Load game", "Options"};
         }
         public static void UcitajPozadinu(ContentManager content)
         {
             teksturaMenija = content.Load<Texture2D>("menu");
-
+            menuItemsFont = content.Load<SpriteFont>("Tahoma");
         }
         public override void Draw(SpriteBatch spritebatch)
         {
             // za ovaj VIEW levi-gore ugao je 0,0 
             spritebatch.Draw(teksturaMenija, new Rectangle(0, 0, base.horizontalSize, base.verticalSize), Color.White);
-        }
+            for (int i = 0; i < this.menuItems.Length; i++)
+                {
+                spritebatch.DrawString(menuItemsFont, menuItems[i], new Vector2(40, this.verticalSize - 150 + i * 25), Color.White);
+                }
+            }
 
         public override void Update(GameTime gameTime)
         {
--- SpaceExplorer/SpaceExplorer/Game/Player.cs
+++ SpaceExplorer/SpaceExplorer/Game/Player.cs
@@ -46,13 +46,17 @@ namespace SpaceExplorer.Game
                 { if (keystate.IsKeyDown(Keys.Enter) && lastKeyboardState.IsKeyUp(Keys.Enter)) { SpawnShip(PlayerIndex.One); } }
             if (playerIndex == PlayerIndex.One)
                 {
-                    //Ship.Direction = new Vector2(0, 0);
-                    if (keystate.IsKeyUp(Keys.Up)) { Config.currentSpeed -= 0.2f; this.Ship.PomeriSe(); }
-                    if (keystate.IsKeyDown(Keys.A)) { this.Ship.Makac(); }
-                    if (keystate.IsKeyDown(Keys.Up)) { Config.currentSpeed += 0.3f; this.Ship.PomeriSe(); }
-                    if (keystate.IsKeyDown(Keys.Down)) { Config.currentSpeed -= 0.3f; this.Ship.PomeriSe(); }//sada radi duplo u stvari
-                    if (keystate.IsKeyDown(Keys.Left))  { this.Ship.rotationAngle -= 0.05f; }
-                    if (keystate.IsKeyDown(Keys.Right)) { this.Ship.rotationAngle += 0.05f; }
+                    if (Config.TrenutniPogledi[0] is GalaxyView || Config.TrenutniPogledi[0] is SistemView)
+                    {
+                        //Ship.Direction = new Vector2(0, 0);
+                        if (keystate.IsKeyUp(Keys.Up)) { Config.currentSpeed -= 0.2f; this.Ship.PomeriSe(); }
+                        if (keystate.IsKeyDown(Keys.A)) { this.Ship.Makac(); }
+                        if (keystate.IsKeyDown(Keys.Up)) { Config.currentSpeed += 0.3f; this.Ship.PomeriSe(); }
+                        if (keystate.IsKeyDown(Keys.Down)) { Config.currentSpeed -= 0.3f; this.Ship.PomeriSe(); }//sada radi duplo u stvari
+                        if (keystate.IsKeyDown(Keys.Left)) { this.Ship.rotationAngle -= 0.05f; }
+                        if (keystate.IsKeyDown(Keys.Right)) { this.Ship.rotationAngle += 0.05f; }
+                    }
+                    
                 }
             lastKeyboardState = keystate;
         }   
